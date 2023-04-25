8d3f14ea79bc6f053041b8d45264f6b5e3af774d
bttf <2374625+bttf@users.noreply.github.com>
2023-04-25 18:53:20 -0400

    first implementation

---
+++ src/app/components/Camera.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { SandboxCamera } from "@/lib/cameraWorld";
+import clsx from "clsx";
+import { FC, useEffect, useRef } from "react";
+
+const Camera: FC<{ camera: SandboxCamera }> = ({ camera }) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const canvas: HTMLCanvasElement = camera.canvas;
+
+  useEffect(() => {
+    if (ref.current && canvas && !ref.current.contains(canvas)) {
+      ref.current.appendChild(canvas);
+    }
+  }, [canvas]);
+
+  return (
+    <div
+      ref={ref}
+      className={clsx("my-2", "h-80", "w-80", "rounded", "overflow-hidden")}
+    ></div>
+  );
+};
+
+export default Camera;
